dcce3daa63d135b31cb546c89c73068ac44a2f1a
prishaa7 <prishaanand9@gmail.com>
2025-03-08 12:36:34 +0530

    Create q2.c

---
+++ q2.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+  int a, b, sum, diff, prod, quo;
+  printf("Enter a:");
+  scanf("%d", &a);
+  printf("Enter b:");
+  scanf("%d", &b(;
+  sum = a + b;
+  diff = a - b;
+  prod = a * b;
+  quo = a / b;
+  printf("The sum is:", sum);
+  printf("The difference is:", diff);
+  printf("The prod is:", prod);
+  printf("The quotient is:", quo);
+  return 0;
+}
